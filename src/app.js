2d2c27d6d16469d5084e900bf3e9ab8060b126bd
Raghu-Ch <raghu6004@gmail.com>
2018-07-14 13:07:49 -0400

    Class Basic, Inheritance & Data Service

--- src/app.js
+++ src/app.js
@@ -4,9 +4,32 @@ import {
 import {
     Drone
 } from './classes/drone.js';
+import {
+    fleet
+} from './fleet-data.js';
+import {
+    FleetDataService
+} from './services/fleet-data-service.js';
 
-let c = new Car();
-let d = new Drone();
+let dataService = new FleetDataService();
+dataService.loadData(fleet);
 
-console.log(c);
-console.log(d);
+// for (let car of dataService.cars) {
+//     console.log(car.license);
+// }
+
+// for (let e of dataService.errors) {
+//     console.log(e.message);
+// }
+
+// let car = dataService.getCarByLicense('AT9900');
+
+// let cars = dataService.getCarsSortedByLicense();
+// for (let car of cars) {
+//     console.log(car.license);
+// }
+
+let cars = dataService.filterCarsByMake('b');
+for (let car of cars) {
+    console.log(car.make);
+}
---
+++ src/fleet-data.js
@@ -0,0 +1,73 @@
+export let fleet = [{
+        license: 'ABC123',
+        type: 'drone',
+        model: 'Amazon 1254',
+        airTimeHours: '6050',
+        base: 'New York',
+        latLong: '40.775596 -73.974615'
+    },
+    {
+        license: 'XYZ478',
+        type: 'drone',
+        model: 'Amazon 1554',
+        airTimeHours: '2100',
+        base: 'New York',
+        latLong: '40.771956 -73.978531'
+    },
+    {
+        license: 'PQR823',
+        type: 'drone',
+        model: 'Google 3900',
+        airTimeHours: '600',
+        base: 'New York',
+        latLong: '40.779423 -73.969411'
+    },
+    {
+        license: 'AT9900',
+        type: 'car',
+        make: 'Tesla',
+        model: 'Quick Transport',
+        miles: '15600',
+        latLong: '40.773272 -73.968875'
+    },
+    {
+        license: 'JX2018',
+        type: 'car',
+        make: 'Tata',
+        model: 'Quick Transport',
+        miles: '23600',
+        latLong: '40.733252 -73.928375'
+    },
+    {
+        license: 'FR2000',
+        type: 'car',
+        make: 'Ford',
+        model: 'Sport',
+        miles: '1600',
+        latLong: '40.775272 -73.963475'
+    },
+    {
+        license: 'UB1019',
+        type: 'car',
+        make: 'Uber',
+        model: 'Zip Trip',
+        miles: '3600',
+        latLong: '40.778878 -73.963435'
+    },
+    {
+        license: 'LY1319',
+        type: 'car',
+        make: 'Lyft',
+        model: 'Lyft Pool',
+        miles: '13400',
+        latLong: '40.775872 -73.963575'
+    },
+    {
+        license: 'UB6004',
+        type: 'car',
+        make: 'Uber',
+        model: 'Pick You Up',
+        miles: '600',
+        latLong: '40.774036 -73.967319'
+    }
+];
